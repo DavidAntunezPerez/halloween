ec07de16a47b6826a1dda8f63a99526a862315e2
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-08 13:42:47 +0100

    Added aditional information

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,5 +1,7 @@
 /****AQUI ESTAN LAS FUNCIONES DE LA WEBPAGE */
 /** Funcion mostrar */
+/** Una función es un módulo que ejecuta una sola tarea, eligiendo bien su nombre podremos ejecutar correctamente nuestras funciones: show -> Muestra, hide -> Oculta... JS toma como objetivo principal document, que es la página html actual, tiene una serie de operadores, llamados getters, que seleccionan elementos para su manipulación. Un ejemplo de getter es : getElementById . Un elemento, seleccionado con un getter puede ser modificado: contenido, estilo, atributos... */
+
 function show (batswindow){
     document.getElementById(batswindow).style.visibility="visible";} // al clickar salen los bats
 function show (ghost){
