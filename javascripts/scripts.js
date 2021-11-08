35f31b9eec9554cc659c4bfc0e1f8cede8adaa09
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-08 14:12:56 +0100

    Added interactive and background audio

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -2,14 +2,19 @@
 /** Funcion mostrar */
 /** Una función es un módulo que ejecuta una sola tarea, eligiendo bien su nombre podremos ejecutar correctamente nuestras funciones: show -> Muestra, hide -> Oculta... JS toma como objetivo principal document, que es la página html actual, tiene una serie de operadores, llamados getters, que seleccionan elementos para su manipulación. Un ejemplo de getter es : getElementById . Un elemento, seleccionado con un getter puede ser modificado: contenido, estilo, atributos... */
 
-function show (batswindow){
+function show(batswindow){
     document.getElementById(batswindow).style.visibility="visible";} // al clickar salen los bats
-function show (ghost){
+function show(ghost){
     document.getElementById(ghost).style.visibility="visible";} // al clickar sale el fantasma
-function show (pumpkin){
+function show(pumpkin){
     document.getElementById(pumpkin).style.visibility="visible";} // muestra las letras al acercar el raton
 function hide(pumpkin){
-    document.getElementById(pumpkin).style.visibility="hidden";}    // oculta las letras al sacar el raton
+    document.getElementById(pumpkin).style.visibility="hidden";} // oculta las letras al sacar el raton
 
-function sound (bgmusic){
-    document.getElementById(bgmusic).audio.play();}
+function sound(bgmusic){
+    document.getElementById(bgmusic).audio.play();} // background music
+
+function sound(bats){
+    document.getElementById(bats).audio.play();} // bats sound
+
+
