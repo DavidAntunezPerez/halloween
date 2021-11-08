029599213aecbbeae6099388c789c146375e960f
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-08 22:27:37 +0100

    Added interactive audio

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -4,15 +4,18 @@
 
 function show(batswindow){
     document.getElementById(batswindow).style.visibility="visible";} // al clickar salen los bats
+
 function show(ghost){
     document.getElementById(ghost).style.visibility="visible";} // al clickar sale el fantasma
+
 function show(pumpkin){
     document.getElementById(pumpkin).style.visibility="visible";} // muestra las letras al acercar el raton
+
 function hide(pumpkin){
     document.getElementById(pumpkin).style.visibility="hidden";} // oculta las letras al sacar el raton
 
 function sound(batssound){
-    document.getElementById(batssound).audio.play();
-} // bats sound
+    document.getElementById(batssound).play(); // bats sound
+} 
 
 
