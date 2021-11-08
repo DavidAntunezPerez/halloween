6904e0fbb3385a1ca48258fc34c6222b20b66bca
DavidAntunezPerez <antunezdavid2003@gmail.com>
2021-11-08 13:40:00 +0100

    Update with more interactions

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,12 +1,13 @@
 /****AQUI ESTAN LAS FUNCIONES DE LA WEBPAGE */
 /** Funcion mostrar */
 function show (batswindow){
-    document.getElementById(batswindow).style.visibility="visible";
-}
-function sound (bgmusic){
-    document.getElementById(bgmusic).audio.play();
-}
+    document.getElementById(batswindow).style.visibility="visible";} // al clickar salen los bats
 function show (ghost){
-    document.getElementById(ghost).style.visibility="visible";}
+    document.getElementById(ghost).style.visibility="visible";} // al clickar sale el fantasma
 function show (pumpkin){
-    document.getElementById(pumpkin).style.visibility="visible";}
+    document.getElementById(pumpkin).style.visibility="visible";} // muestra las letras al acercar el raton
+function hide(pumpkin){
+    document.getElementById(pumpkin).style.visibility="hidden";}    // oculta las letras al sacar el raton
+
+function sound (bgmusic){
+    document.getElementById(bgmusic).audio.play();}
